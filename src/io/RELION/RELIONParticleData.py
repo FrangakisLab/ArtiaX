9d26994d680eec0f4a3942840f7cf91abfe0bfe9
uermel <uermel@czii.org>
2026-06-16 15:38:47 -0700

    use logger for printing

--- src/io/RELION/RELIONParticleData.py
+++ src/io/RELION/RELIONParticleData.py
@@ -141,8 +141,9 @@ class RELIONParticleData(ParticleData):
             if "rlnCoordinateZ" in list(val.keys()):
                 data_loop = key
                 break
-            elif 'wrpCoordinateZ1' in list(val.keys()): # Check whether it's a Warp/M starfile and if so, edit parameter names.
-                print(f"Changing Warp/M starfile columns to Relion format.")
+            elif "wrpCoordinateZ1" in list(val.keys()):
+                # Warp/M starfile: rename the relevant wrp* columns to their rln* equivalents.
+                self.session.logger.info("Changing Warp/M starfile columns to RELION format.")
                 wrp_to_rln = [('wrpCoordinateX1', 'rlnCoordinateX'),
                               ('wrpCoordinateY1', 'rlnCoordinateY'),
                               ('wrpCoordinateZ1', 'rlnCoordinateZ'),
@@ -151,9 +152,10 @@ class RELIONParticleData(ParticleData):
                               ('wrpAnglePsi1', 'rlnAnglePsi'), ]
                 for old, new in wrp_to_rln:
                     if old in val.columns:
-                        print(f'renaming {old} to {new}')
+                        self.session.logger.info("Renaming {} to {}.".format(old, new))
                         val.rename(columns={old: new}, inplace=True)
                 data_loop = key
+                break
 
         # Abort if none found
         if data_loop is None:
@@ -285,8 +287,11 @@ class RELIONParticleData(ParticleData):
                     num = int(n[-1])
                     p["rlnTomoName"] = num
                 except Exception as e:
-                    "" if _raised_tomoname_parse_error else print(f'Could not parse rlnTomoName the original way - applying workaround.')
-                    _raised_tomoname_parse_error = True
+                    if not _raised_tomoname_parse_error:
+                        self.session.logger.warning(
+                            "Could not parse rlnTomoName the original way - applying workaround."
+                        )
+                        _raised_tomoname_parse_error = True
                     p["rlnTomoName"] = int(''.join(filter(str.isdigit, row["rlnTomoName"])))
 
             # Position
